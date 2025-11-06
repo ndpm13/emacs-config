d1f4b5495dd46f4ea2930254211d7a4634f4dead
Naz <ndpm13@ch-naseem.com>
2025-11-06 22:12:44 +0100

    💩dirty commit with my current config

---
+++ scripts/doom-functions.el
@@ -0,0 +1,9 @@
+(defun delete-other-windows-horizontally ()
+  "Delete all windows to the left and right of the current window."
+  (interactive)
+  (require 'windmove)
+  (save-excursion
+    (while (ignore-errors (windmove-left)) (delete-window))
+    (while (ignore-errors (windmove-right)) (delete-window))))
+
+(provide 'doom-functions)
